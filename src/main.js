fe4c19b19c8da8a50cc90fc8a6e95c6f56a3637c
coCo🙊 <72194813+kachi33@users.noreply.github.com>
2025-07-29 17:38:18 +0100

    Implement real-time input value retrieval for name and number field

--- src/main.js
+++ src/main.js
@@ -1,1 +1,18 @@
 import './style.css'
+
+const nameInput = document.getElementById('name');
+nameInput.addEventListener('input', (e) => {
+  const nameValue = e.target.value;
+
+  document.getElementById('card-name-display').textContent = nameValue;
+  console.log(nameValue);
+});
+
+const nunmberInput = document.getElementById('number');
+nunmberInput.addEventListener('input', (e) => {
+  const numberValue = e.target.value;
+
+  document.getElementById('card-number-display').textContent = numberValue;
+  console.log(numberValue); 
+});
+
